bab5fb056e4a1e46432163870666c2c3a2a0c703
pranith-mk <mkpranith007@gmail.com>
2025-06-24 15:52:03 +0530

    created ui

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Bmi extends StatelessWidget {
+class Bmi extends StatefulWidget {
   const Bmi({super.key});
 
+  @override
+  State<Bmi> createState() => _BmiState();
+}
+
+class _BmiState extends State<Bmi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
